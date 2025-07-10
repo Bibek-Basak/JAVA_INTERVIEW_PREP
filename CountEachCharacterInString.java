0be97ec986f53afeef1fcd99d1d626b7b2ce422d
Bibek Basak <82058568+Bibek-Basak@users.noreply.github.com>
2025-07-10 05:13:27 +0100

    Update CountEachCharacterInString.java

--- CountEachCharacterInString.java
+++ CountEachCharacterInString.java
@@ -8,7 +8,7 @@ public class CountEachCharacterInString {
 
 	public static void main(String[] args) {
 		String s1 = "I am a good boy";
-		Map<Character, Long> count = s1.chars().mapToObj(ch -> (char)ch)
+		Map<Character, Long> count = s1.chars().filter(chr -> chr != ' ').mapToObj(ch -> (char)ch)
 		.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 		System.out.println(count);
 	}
